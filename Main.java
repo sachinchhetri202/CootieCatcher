d1d820a4cc4226fb007040e6f7a1beb0cf6a6aef
Sachin Chhetri <71232069+sachinchhetri202@users.noreply.github.com>
2021-04-08 22:03:31 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,38 +4,37 @@
     * Use of Array, Loops, try/catch, and Conditions.  
     */
 
-import java.util.*;
+import java.util.Scanner;
 
-public class Main{
-   
-   public static void main(String[] args){
-      Cootie c = new Cootie(); // another Class
-      Scanner in = new Scanner(System.in);
-      
-      System.out.println("Cootie Catcher");
-      
-      try{
-         while(true){
-            System.out.print("Ask your Cootie Catcher a question: ");
-            String a = in.nextLine(); // user enters the question
-            c.makeChoice();
-            System.out.print("Do you want to continue? y/n : ");
-            String quit = in.nextLine();
-            if (quit.equalsIgnoreCase("n")){
-               break;
+public class Main {
+    public static void main(String[] args){
+        Cootie c = new Cootie();
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Cootie Catcher");
+        
+        // handling anykind of exception that the program might throw
+        try{
+            while (true){
+                c.makeChoice();
+                System.out.print("Do you want to continue? y/n : ");
+                String quit = in.nextLine();
+                if (quit.equalsIgnoreCase("n")){
+                    break;
+                }
+                else if(quit.equalsIgnoreCase("y")){
+                    continue;
+                }
+                else{
+                    System.out.println("Sorry Invalid Input.");
+                    break;
+                }
             }
-            else if(quit.equalsIgnoreCase("y")){
-               continue;
-            }
-            else{
-               System.out.println("Sorry Invalid Input.");
-               break;
-            }
-         }
-      }
-      catch (Exception e){
-         System.out.println("Invalid Input. Try Again.");
-      }
-      in.close();  
-   }
+        }
+        catch(Exception e){
+            System.out.println("Invalid Input. Try Again");
+        }
+        in.close();
+    }   
 }
+
